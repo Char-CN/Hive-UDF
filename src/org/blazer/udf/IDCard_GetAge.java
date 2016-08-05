dd153e1d351019b40d2326a9bc7d0b605bc9aede
hyy <hyy@192.168.99.1>
2016-08-05 16:46:32 +0800

    commit

--- src/org/blazer/udf/IDCard_GetAge.java
+++ src/org/blazer/udf/IDCard_GetAge.java
@@ -8,19 +8,18 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 public class IDCard_GetAge extends UDF {
 
 	public Integer evaluate(String idcard) throws Exception {
-		String birthday = null;
+		Integer birthday = null;
 		if (StringUtils.isBlank(idcard)) {
 			return null;
 		}
 		if (idcard.length() == 18) {
 			try {
-				birthday = "" + Integer.parseInt(idcard.substring(6, 14));
+				birthday = Integer.parseInt(idcard.substring(6, 14));
 			} catch (Exception e) {
 			}
-
 		} else if (idcard.length() == 15) {
 			try {
-				birthday = "19" + Integer.parseInt(idcard.substring(6, 12));
+				birthday = Integer.parseInt("19" + idcard.substring(6, 12));
 			} catch (Exception e) {
 			}
 		}
@@ -30,26 +29,24 @@ public class IDCard_GetAge extends UDF {
 		return calcAge(birthday);
 	}
 
+	private int calcAge(int birthday) {
+		Calendar c = Calendar.getInstance();
+		String y = "" + c.get(Calendar.YEAR);
+		int m = c.get(Calendar.MONTH);
+		int d = c.get(Calendar.DAY_OF_MONTH);
+		int today = Integer.parseInt(y + (m < 10 ? "0" + m : m) + (d < 10 ? "0" + d : d));
+		if (today - birthday < 10000)
+			return 0;
+		return (today - birthday) / 10000;
+	}
+
 	public static void main(String[] args) throws Exception {
 		IDCard_GetAge s = new IDCard_GetAge();
 		System.out.println(s.evaluate("130503900509001"));
 		System.out.println(s.evaluate("130503900101001"));
 		System.out.println(s.evaluate("130503901231001"));
-	}
-
-	private static int calcAge(String birthday) {
-		Integer birthYear = Integer.parseInt(birthday.substring(0, 4));
-		Integer birthMonth = Integer.parseInt(birthday.substring(4, 6));
-		Integer birthDay = Integer.parseInt(birthday.substring(6, 8));
-		Calendar c = Calendar.getInstance();
-		int nowYear = c.get(Calendar.YEAR);
-		int nowMonth = c.get(Calendar.MONTH) + 1;
-		int nowDay = c.get(Calendar.DAY_OF_MONTH);
-		int add = 0;
-		if (birthMonth < nowMonth || (birthMonth == nowMonth && birthDay <= nowDay)) {
-			add = -1;
-		}
-		return nowYear - birthYear + add;
+		System.out.println(s.evaluate("430211199007020000"));
+		System.out.println(s.evaluate("430211198207020000"));
 	}
 
 }
